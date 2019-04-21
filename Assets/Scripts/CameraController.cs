e3e1b47de69eb70faea38765890f458deb57adef
Masahiro Atsumi <raindro.aqua@gmail.com>
2019-04-21 17:10:38 +0900

    初回コミット

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Camera DisposeCamera;
+    private Canvas _canvas;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        this._canvas = this.GetComponent<Canvas>();
+
+        if (Camera.main != null
+            && this.DisposeCamera != null
+            && !Object.ReferenceEquals(this.DisposeCamera, Camera.main)) {
+
+            this._canvas.worldCamera = Camera.main;
+            Destroy(this.DisposeCamera.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    private string beforeSceneName;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void PushButton(string name) {
+        UnloadScene(name);
+        SceneManager.LoadScene("Scene" + name, LoadSceneMode.Additive);
+    }
+
+    public void UnloadScene(string name) {
+        if (!string.IsNullOrEmpty(this.beforeSceneName)) {
+            SceneManager.UnloadSceneAsync("Scene" + beforeSceneName);
+        }
+        this.beforeSceneName = name;
+    }
+}
